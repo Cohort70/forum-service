0211b7ba25b76948c0ad2f7f9ed023be29df93c0
edward <edwardlfish@gmail.com>
2025-12-17 14:03:38 +0200

    Add Spring Security configuration with `SecurityFilterChain` and update `UserDetailsServiceImpl` to use raw password

--- src/main/java/ait/cohort70/security/UserDetailsServiceImpl.java
+++ src/main/java/ait/cohort70/security/UserDetailsServiceImpl.java
@@ -24,6 +24,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                 .stream()
                 .map(role -> "ROLE_" + role.name())
                 .toList();
-        return new User(username, "{noop}" + userAccount.getPassword(), AuthorityUtils.createAuthorityList(roles));
+        return new User(username, userAccount.getPassword(), AuthorityUtils.createAuthorityList(roles));
     }
 }
