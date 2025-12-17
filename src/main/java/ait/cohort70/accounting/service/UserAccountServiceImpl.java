028692c57904af4be5a73e91b30e41d44ff9d0b0
edward <edwardlfish@gmail.com>
2025-12-17 13:50:13 +0200

    Integrate Spring Security with BCrypt password encoding, implement custom `UserDetailsService`, and update MongoDB-based user registration and password management.

--- src/main/java/ait/cohort70/accounting/service/UserAccountServiceImpl.java
+++ src/main/java/ait/cohort70/accounting/service/UserAccountServiceImpl.java
@@ -11,6 +11,7 @@ import ait.cohort70.accounting.dto.exception.UserNotFoundException;
 import ait.cohort70.accounting.model.UserAccount;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Service;
 public class UserAccountServiceImpl implements UserAccountService {
     private final UserAccountRepository userAccountRepository;
     private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDto register(UserRegisterDto userRegisterDto) {
@@ -26,6 +28,8 @@ public class UserAccountServiceImpl implements UserAccountService {
         }
         UserAccount userAccount = modelMapper.map(userRegisterDto, UserAccount.class);
         userAccount.addRole("USER");
+        String encodedPassword = passwordEncoder.encode(userRegisterDto.getPassword());
+        userAccount.setPassword(encodedPassword);
         userAccountRepository.save(userAccount);
         return modelMapper.map(userAccount, UserDto.class);
     }
@@ -83,7 +87,8 @@ public class UserAccountServiceImpl implements UserAccountService {
     @Override
     public void changePassword(String login, String newPassword) {
         UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
-        userAccount.setPassword(newPassword);
+        String encodedPassword = passwordEncoder.encode(newPassword);
+        userAccount.setPassword(encodedPassword);
         userAccountRepository.save(userAccount);
     }
 }
