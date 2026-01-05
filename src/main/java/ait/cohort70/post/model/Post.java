8242233942adf867c8f7e5cf1ec6e59987817e9b
edward <edwardlfish@gmail.com>
2026-01-05 10:47:24 +0200

    Add cascading delete for `Post` entity, update foreign key in `comments` table with `CASCADE` rule, and enable `orphanRemoval` for comments

--- src/main/java/ait/cohort70/post/model/Post.java
+++ src/main/java/ait/cohort70/post/model/Post.java
@@ -42,7 +42,7 @@ public class Post {
     private Set<Tag> tags = new HashSet<>();
     @Column(name = "likes")
     private int likes;
-    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "post", orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
 
     public Post(String title, String content, String author) {
